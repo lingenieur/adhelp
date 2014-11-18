b4020e2924fecd61fedb80664e4aaae4ea10f605
Noitidart <noitidart@gmail.com>
2014-11-18 09:22:16 -0800

    Update browserOverlay.js
    append it so its really over everything

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -28,13 +28,13 @@ adhelpChrome1.BrowserOverlay = {
     var props = {
       style: 'position:fixed; top:0; left:0; width:300px; height:300px; opacity:0.6; background-color:steelblue; z-index:999999',
       id: 'adhelp-iframe',
-      src: 'data:text/html,does this work?', /*'chrome://adhelp/content/page.html', /*'data:text/html,<link media="screen" rel="stylesheet" href="jquery-ui.css" /> <link media="screen" rel="stylesheet" href="chrome://adhelp/content/jquery-ui.structure.css" /> <link media="screen" rel="stylesheet" href="chrome://adhelp/content/jquery-ui.theme.css" /> <script src="chrome://adhelp/content/jquery.min.js" type="text/javascript"></script> <script src="chrome://adhelp/content/jquery-ui.min.js" type="text/javascript"></script><script>alert("loaded")</script>hi'*/
+      src: 'data:text/html,does this work', /*'chrome://adhelp/content/page.html', /*'data:text/html,<link media="screen" rel="stylesheet" href="jquery-ui.css" /> <link media="screen" rel="stylesheet" href="chrome://adhelp/content/jquery-ui.structure.css" /> <link media="screen" rel="stylesheet" href="chrome://adhelp/content/jquery-ui.theme.css" /> <script src="chrome://adhelp/content/jquery.min.js" type="text/javascript"></script> <script src="chrome://adhelp/content/jquery-ui.min.js" type="text/javascript"></script><script>alert("loaded")</script>hi'*/
       type: 'content'
     };
     for (var p in props) {
       iframe.setAttribute(p, props[p]);
     }
-    contentDocument.documentElement.insertBefore(iframe, contentDocument.documentElement.firstChild);
+    contentDocument.documentElement.appendChild(iframe); //insertBefore(iframe, contentDocument.documentElement.firstChild);
 
   }
 };
