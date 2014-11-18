40b48d6149b0bf8ec632e55c309dbc18cf4651c8
Noitidart <noitidart@gmail.com>
2014-11-18 10:16:14 -0800

    Update browserOverlay.js
    ok transparent bg its working now

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -22,7 +22,7 @@ adhelpChrome1.BrowserOverlay = {
     var deck = document.createElementNS('http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul', 'deck');
     var props = {
       class: 'adhelp-deck',
-      style: 'background-color:steelblue;'
+      /*style: 'background-color:steelblue;'*/
     };
     for (var p in props) {
       deck.setAttribute(p, props[p]);
@@ -38,7 +38,7 @@ adhelpChrome1.BrowserOverlay = {
       }, false);
     }, false);
     var props = {
-      style: 'border:0; width:100%; height:100%; background-color:springgreen;',
+      style: 'border:0; width:100%; height:100%;',
       class: 'adhelp-iframe',
       src: 'chrome://adhelp/content/page.html'
     };
