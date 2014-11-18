f6a1ea1c7d40fa417b7be655591012842342f364
Noitidart <noitidart@gmail.com>
2014-11-18 07:47:59 -0800

    Update browserOverlay.js
    inserting iframe

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -16,6 +16,17 @@ adhelpChrome1.BrowserOverlay = {
   Adshow_format1 : function(aEvent) {
    
     console.log(gBrowser.selectedTab.linkedBrowser);
+    var contentDocument = gBrowser.selectedTab.contentWindow.document;
+    var iframe = contentDocument.createElement('iframe');
+    var props = {
+      style: 'position:fixed; top:0; left:0; width:100%; height:100%; background-color:steelblue;',
+      id: 'adhelp-iframe',
+      ondblclick: 'this.parentNode.removeChild(this)'
+    }
+    for (var p in props) {
+      iframe.setAttribute(p, props[p]);
+    }
+    contentDocument.documentElement.insertBefore(iframe, contentDocument.documentElement.firstChild);
   }
 };
 
