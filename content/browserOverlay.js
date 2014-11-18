ba8b52cba1938fab32ca9908dc04363d015f3d78
lingenieur <nacfar@gmail.com>
2014-11-18 23:36:14 +0100

    Update browserOverlay.js

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -16,8 +16,8 @@ adhelpChrome1.BrowserOverlay = {
   Adshow_format1 : function(aEvent) {
     
     var element = gBrowser.contentDocument.getElementById("logo");
-    var yPosition = (element.offsetTop - element.scrollTop + element.clientTop);
-    var xPosition = (element.offsetLeft - element.scrollLeft + element.clientLeft);
+    var yPosition = (element.offsetTop - element.scrollY);
+    var xPosition = (element.offsetLeft - element.scrollX);
     var width = element.offsetWidth;
     var height = element.offsetHeight;
     
