4f68d108a585806282ca9fe96e134e1d48961e0d
Noitidart <noitidart@gmail.com>
2014-11-18 07:38:40 -0800

    Update browserOverlay.js
    testing scope to gBrowser

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -15,17 +15,7 @@ adhelpChrome1.BrowserOverlay = {
    */
   Adshow_format1 : function(aEvent) {
    
-    let height = 300;
-	let width = 300;
-	let percent_width = 0.90;
-	let percent_height = 0.59;
-	let wt = (screen.width - width) * percent_width;
-	let hg = (screen.height - height) * percent_height;
-	let features = "height =" + height + "px, width =" + width + "px, left =" + wt + ", top =" + hg;
-	window.open(
-    "chrome://adhelp/content/page_format1.xul",
-    "adhelp-pub-format1",
-    "chrome," +features+ ", resizable=no,scrollbars=no,status=no,titlebar=no,toolbar=no,menubar=no,location=no,dependent=yes,modal=no,dialog=no,close=no");
+    console.log(gBrowser.selectedTab.linkedBrowser);
   }
 };
 
