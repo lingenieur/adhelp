f59e795d1ee21b3a1659878d5a6c531fca4e6bef
Noitidart <noitidart@gmail.com>
2014-11-18 09:24:43 -0800

    Update browserOverlay.js
    trying http://www.w3.org/1999/xhtml to get load event

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -17,8 +17,8 @@ adhelpChrome1.BrowserOverlay = {
    
     console.log(gBrowser.selectedTab.linkedBrowser);
     var contentDocument = gBrowser.selectedTab.linkedBrowser.contentWindow.document;
-    var iframe = contentDocument.createElement('iframe');
-    iframe.addEventListener('load', function(e) {
+    var iframe = contentDocument.createElementNS('http://www.w3.org/1999/xhtml', 'iframe');
+    iframe.addEventListener('DOMContentLoaded', function(e) {
       var iframeContentWindow = e;
       console.log('iframeContentWindow, e:', e);
     }, false);
@@ -28,7 +28,7 @@ adhelpChrome1.BrowserOverlay = {
     var props = {
       style: 'position:fixed; top:0; left:0; width:300px; height:300px; opacity:0.6; background-color:steelblue; z-index:999999',
       id: 'adhelp-iframe',
-      src: 'data:text/html,does this work', /*'chrome://adhelp/content/page.html', /*'data:text/html,<link media="screen" rel="stylesheet" href="jquery-ui.css" /> <link media="screen" rel="stylesheet" href="chrome://adhelp/content/jquery-ui.structure.css" /> <link media="screen" rel="stylesheet" href="chrome://adhelp/content/jquery-ui.theme.css" /> <script src="chrome://adhelp/content/jquery.min.js" type="text/javascript"></script> <script src="chrome://adhelp/content/jquery-ui.min.js" type="text/javascript"></script><script>alert("loaded")</script>hi'*/
+      src: 'http://www.bing.com/', /*'chrome://adhelp/content/page.html', /*'data:text/html,<link media="screen" rel="stylesheet" href="jquery-ui.css" /> <link media="screen" rel="stylesheet" href="chrome://adhelp/content/jquery-ui.structure.css" /> <link media="screen" rel="stylesheet" href="chrome://adhelp/content/jquery-ui.theme.css" /> <script src="chrome://adhelp/content/jquery.min.js" type="text/javascript"></script> <script src="chrome://adhelp/content/jquery-ui.min.js" type="text/javascript"></script><script>alert("loaded")</script>hi'*/
       type: 'content'
     };
     for (var p in props) {
